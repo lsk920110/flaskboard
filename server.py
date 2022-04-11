7f73a0f4237a1415af2fbfd8d8e7d0aee9b1a32f
lee seung kyu <lsk920110@gmail.com>
2022-04-11 23:13:48 +0900

    routing 요청 처리하기

--- server.py
+++ server.py
@@ -1,10 +1,23 @@
 from flask import Flask
+import random
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h5>debugmode</h5>'
+    return 'Welcome'
+
+@app.route('/create/')
+def create():
+    return 'Create'
+
+@app.route('/read/<id>/')
+def read(id):
+    print(id)
+    return 'Read '+id
+
+
 
 app.run(port=5001 , debug=True) 
 #python server.py 으로 터미널에서 실행
