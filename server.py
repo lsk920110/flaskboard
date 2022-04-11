dadb411afb4088e39405b0a7c0c05595c9eaf0fe
lee seung kyu <lsk920110@gmail.com>
2022-04-11 23:44:41 +0900

    읽기 기능 구현 코드 함수로 정리

--- server.py
+++ server.py
@@ -12,34 +12,47 @@ topics = [
 ]
 
 
-
-@app.route('/')
-def index():
-    liTags = ''
-    for topic in topics:
-        liTags = liTags + f'<li><a href="/read/{topic["id"]}">{topic["title"]}</a></li>'
-
+def template(contents,content):
     return f'''<!doctype html>
     <html>
         <body>
             <h1><a href="/">WEB</a><h1>
             <ol>
-                {liTags}
+                {contents}
             </ol>
-            <h2>Welcome</h2>
-            Hello, Web
+            {content}
         </body>
     </html>    
     '''
+def getContents():
+    liTags = ''
+    for topic in topics:
+        liTags = liTags + f'<li><a href="/read/{topic["id"]}">{topic["title"]}</a></li>'
+    return liTags
+
+
+
+@app.route('/')
+def index():
+
+
+    return template(getContents(), '<h2>Welcome</h2>Hello,WEB')  
+    
 
 @app.route('/create/')
 def create():
     return 'Create'
 
-@app.route('/read/<id>/')
+@app.route('/read/<int:id>/')
 def read(id):
-    print(id)
-    return 'Read '+id
+
+    
+    for topic in topics:
+        if id == topic['id']:
+            title = topic['title']
+            body = topic['body']
+            break
+    return template(getContents(), f'<h2>{title}</h2>{body}')
 
 
 
